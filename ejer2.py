7f88d26e5c29d3ebe22cd99baaee92b9efdb03e5
Alberto Rivas <alberto.rivas2264@alumnos.udg.mx>
2023-10-20 20:17:38 -0600

    Agregar segundo ejercicio

---
+++ ejer2.py
@@ -0,0 +1,28 @@
+from gpiozero import MCP3008, DistanceSensor, Button, LED
+from time import sleep
+from datetime import datetime
+
+pot = MCP3008(7)
+ultrasonic = DistanceSensor(echo=21, trigger=20)
+button = Button(18)
+led_rojo = LED(25)
+file = open("/home/team3/Unit_Practice/distance_log.txt.", "w")
+
+while True:
+    if button.when_activated():
+        dist = ultrasonic.distance * 100
+        span = pot.value * 100
+        dist, span = round(dist, 3), round(span, 3)
+        if dist <= span:
+            print(f"scaled span : {span}, dist : {dist}")
+            led_rojo.off()
+            timestamp = datetime.now().strftime('%Y/%m/%d %HH %MM %SS')
+            data = f"{timestamp} ---> " \
+                f"distance : {dist}, span : {span}\n"
+            file.write()
+        else:
+            print(f"Distance > span!! scaled span : {span}, dist : {dist}")
+            led_rojo.on()
+
+    sleep(1)
+file.close()
