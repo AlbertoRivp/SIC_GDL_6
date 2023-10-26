5980381b8a10e711034066904a2ac5ab4737ac69
Alberto Rivas <alberto.rivas2264@alumnos.udg.mx>
2023-10-25 18:25:40 -0600

    Usar wait_for_press() en lugar de when_activated,Quien sabe si funcione xd

--- ejer2.py
+++ ejer2.py
@@ -9,20 +9,20 @@ led_rojo = LED(25)
 file = open("/home/team3/Unit_Practice/distance_log.txt.", "w")
 
 while True:
-    if button.when_activated():
-        dist = ultrasonic.distance * 100
-        span = pot.value * 100
-        dist, span = round(dist, 3), round(span, 3)
-        if dist <= span:
-            print(f"scaled span : {span}, dist : {dist}")
-            led_rojo.off()
-            timestamp = datetime.now().strftime('%Y/%m/%d %HH %MM %SS')
-            data = f"{timestamp} ---> " \
-                f"distance : {dist}, span : {span}\n"
-            file.write()
-        else:
-            print(f"Distance > span!! scaled span : {span}, dist : {dist}")
-            led_rojo.on()
+    button.wait_for_press()
+    dist = ultrasonic.distance * 100
+    span = pot.value * 100
+    dist, span = round(dist, 3), round(span, 3)
+    if dist <= span:
+        print(f"scaled span : {span}, dist : {dist}")
+        led_rojo.off()
+        timestamp = datetime.now().strftime('%Y/%m/%d %HH %MM %SS')
+        data = f"{timestamp} ---> " \
+            f"distance : {dist}, span : {span}\n"
+        file.write()
+    else:
+        print(f"Distance > span!! scaled span : {span}, dist : {dist}")
+        led_rojo.on()
 
     sleep(1)
 file.close()
